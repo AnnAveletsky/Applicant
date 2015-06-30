41ee1412f3c08685b782c5271518b5cab0cd8e0b
Аня Серба <annserba94@gmail.com>
2015-06-30 20:14:39 +0300

    Подклбчение библиотеги fileinput

---
+++ Applicant/Models/Attachment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace Applicant.Models
+{
+    public class Attachment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public Stream Attach { get; set; }
+    }
+}
