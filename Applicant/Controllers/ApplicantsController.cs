14dc64ad0df7cdd1c3d4a99805db63a5835afa52
AnnSerba <annserba94@gmail.com>
2015-07-01 19:52:24 +0400

    Контроллеры и представления генерированные шаблоны

---
+++ Applicant/Controllers/ApplicantsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Applicant.Models;
+
+namespace Applicant.Controllers
+{
+    public class ApplicantsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Applicants
+        public ActionResult Index()
+        {
+            return View(db.Applicants.ToList());
+        }
+
+        // GET: Applicants/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Applicant.Models.Applicant applicant = db.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicant);
+        }
+
+        // GET: Applicants/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Applicants/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AplicantID,FirstName,MiddleName,LastName,Email,Phone,HistoryComments,Salary")] Applicant.Models.Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Applicants.Add(applicant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(applicant);
+        }
+
+        // GET: Applicants/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Applicant.Models.Applicant applicant = db.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicant);
+        }
+
+        // POST: Applicants/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AplicantID,FirstName,MiddleName,LastName,Email,Phone,HistoryComments,Salary")] Applicant.Models.Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(applicant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicant);
+        }
+
+        // GET: Applicants/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Applicant.Models.Applicant applicant = db.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicant);
+        }
+
+        // POST: Applicants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Applicant.Models.Applicant applicant = db.Applicants.Find(id);
+            db.Applicants.Remove(applicant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
