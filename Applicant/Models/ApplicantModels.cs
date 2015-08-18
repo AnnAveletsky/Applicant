ab92409cb0c15d4a13c17f0dc1f5ac8f163f7721
Aveletsky <ixus.van.axel@gmail.com>
2015-08-18 21:23:29 +0300

    Front-end.Details
    Настройка отображения страницы с контактными и личными данными
    соискателя

--- Applicant/Models/ApplicantModels.cs
+++ Applicant/Models/ApplicantModels.cs
@@ -90,7 +90,7 @@ namespace Applicant.Models
 
         [
             DataType(DataType.Currency),
-            Range(1.0, 1000000.0, ErrorMessage = "Необходимо установить от 1 до 1 000 000 руб."),
+            Range(1, 1000000, ErrorMessage = "Необходимо установить от 1 до 1 000 000 руб."),
             Display(Name = "Зарплата")
         ]
         public int Salary { get; set; }
