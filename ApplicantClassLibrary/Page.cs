46a7695799977e91c2f0e8be981f0af835e3497a
Аня Серба <annserba94@gmail.com>
2015-08-27 02:02:47 +0300

    Редактированы мелочи

--- ApplicantClassLibrary/Page.cs
+++ ApplicantClassLibrary/Page.cs
@@ -6,15 +6,31 @@ namespace ApplicantClassLibrary
 {
     public class Page
     {
+        //Текущая страница
         public int NowPage { get; set; }
+
+        //Количество страниц
         public int CountPages { get; set; }
+
+        //Количество выводимых элементов на страницу
         public int CountElementsInPage { get; set; }
+
+        //Строка поиска
         public string Search { get; set; }
+
+        //Поле сортировки
         public string PoleSort { get; set; }
+
+        //Метод сортировки
         public OrderSort OrderSort { get; set; }
+
+        //Следующая страница
         public bool Next { get; set; }
+
+        //Предыдущая страница
         public bool Back { get; set; }
-        public Page(int countApplicants, 
+
+        public Page(int countApplicants,
             int? page = 1, int? countElementsInPage = 5, 
             string poleSort = "FirstName", OrderSort orderSort=OrderSort.Прямой, 
             string search = "")
