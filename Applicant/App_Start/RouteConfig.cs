2c1bd28f8ef5e57388f89633d12e9a7c6ae8b27a
Аня Серба <annserba94@gmail.com>
2015-08-30 20:39:03 +0300

    Убрала начальную страницу и сделала внешние изменения с поиском

--- Applicant/App_Start/RouteConfig.cs
+++ Applicant/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Applicant
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Applicants", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
